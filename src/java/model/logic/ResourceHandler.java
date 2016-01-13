07bdd5b9a7d5b138a63a8f0f903ab689ee06e7aa
RAvidu <ravidu0915@gmail.com>
2016-01-13 22:56:04 +0530

    SearchAll completed

--- src/java/model/logic/ResourceHandler.java
+++ src/java/model/logic/ResourceHandler.java
@@ -5,8 +5,10 @@
  */
 package model.logic;
 
-import data.DBSelectHandler;
+import data.*;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import model.foundation.ResourceView;
 
 /**
  *
@@ -17,6 +19,12 @@ public class ResourceHandler {
     public int getResourceCount() throws SQLException {
       
             return new DBSelectHandler().getResourceCount();
+       
+    }
+    
+     public ArrayList<ResourceView> getResources(int start,int end) throws SQLException {
+      
+            return new DBSearchHandler().searchbyCategory(start, end);
        
     }
 }
